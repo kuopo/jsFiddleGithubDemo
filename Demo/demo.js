35e3731fd73e983bf37fe21d9503ba8da62ad8e3
kuopo <shengpo.kuo@gmail.com>
2014-02-09 08:53:35 +0000

    update

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,22 +1,8 @@
 var MSGHUB_PUBSUB_URL = 'https://pubsub.msghub.io';
 
-$(document).ready(function() {
 var msghub = new MsgHub(MSGHUB_PUBSUB_URL);
 
 msghub.subscribe('success', function (channel, data) {
     console.log(channel);
     console.log(data);
 });
-});
-
-window.addEvent('domready', function() {
-    new Request.HTML({
-        url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
-        data: {'delay': 1},
-        method: 'post',
-        update: 'demo',
-        onSuccess: function(response) {
-            $('demo').highlight();
-        }
-    }).send();
-})
