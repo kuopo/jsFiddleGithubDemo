c6673757f7516b36ba10e3be16cd3b8b5f1e1a21
kuopo <shengpo.kuo@gmail.com>
2014-02-09 08:18:35 +0000

    update

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,9 +1,12 @@
 var MSGHUB_PUBSUB_URL = 'https://pubsub.msghub.io';
+
+$(document).ready(function() {
 var msghub = new MsgHub(MSGHUB_PUBSUB_URL);
 
 msghub.subscribe('success', function (channel, data) {
     console.log(channel);
     console.log(data);
+});
 });
 
 window.addEvent('domready', function() {
